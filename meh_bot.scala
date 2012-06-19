18d36237a91a7238ef0a4dc67f0adbdac5abc227
HairyFotr <hairyfotr@gmail.com>
2012-06-19 22:21:40 +0200

    some fixes

--- meh_bot.scala
+++ meh_bot.scala
@@ -7,17 +7,24 @@ object meh_bot extends App { new meh_bot }
 
 class meh_bot extends PircBot {
     this.setName("_mehbot_")
-    this.setVerbose(true)
+    this.setVerbose(false)
     this.connect("irc.freenode.net")
     val chan = io.Source.fromFile(".channel").getLines.toList(0)
     this.joinChannel(chan)
 
-    def wordbag = io.Source.fromFile("meh_bot.db").getLines.map(_.trim.toLowerCase).toSet
+    def rep(s:String) = s.map(a=>("čćžšđ".zip("cczsd").toMap).getOrElse(a, a))
+    def wordbag = io.Source.fromFile("meh_bot.db")
+            .getLines
+            .map(_.trim.toLowerCase)
+            .flatMap(s => List(s, rep(s)))
+            .toSet
 
     override def onMessage(channel:String, sender:String, login:String, hostname:String, message:String) = {
-        val newWords = message.replaceAll("[^\\pL'_-]", " ").split(" ").map(_.trim.toLowerCase).filter(_.length > 1).toSet
+        val newWords = rep(message).replaceAll("[^\\pL'_-]", " ").split(" ").map(_.trim.toLowerCase).filter(_.length > 1).toSet
         val mehFactors = (wordbag & newWords)
 
+        println(rep(message))
+        println(newWords)
         println(mehFactors)
         if(mehFactors.size>1) speak(.2*mehFactors.size)
     }
