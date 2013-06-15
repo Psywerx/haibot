1226485291718bfc29d1a55b913da7282a72c84a
HairyFotr <hairyfotr@gmail.com>
2013-06-15 10:20:57 +0200

    added some assertions to sms feature

--- NovatelSMS.scala
+++ NovatelSMS.scala
@@ -5,14 +5,21 @@ object NovatelSMS {
     "ok" -> "Ok",
     "auth" -> "Unsuccessful authentication",
     "to_number" -> "Invalid destination number",
-    "no_msg" -> "Missing SMS text"
+    "no_msg" -> "Missing SMS text",
+    "assert" -> "Assertion failed"
   ) withDefaultValue "Unknown error"
 }
 
 class NovatelSMS(username: String, password: String) {
   def sendSMS(msg: String, number: String): String = {
     try {
-      val msg64 = new sun.misc.BASE64Encoder().encode(msg.trim.replaceAll("[^\\p{Print}]", "_").getBytes)
+      //TODO: base64 encode everything, as is recommended by API
+      assert(msg.length <= 500)
+      assert(number matches "(00)?[386][0-9]+")
+      assert(!(username contains "&"))
+      assert(!(password contains "&"))
+      
+      val msg64 = new sun.misc.BASE64Encoder().encode(msg.getBytes)
       
       val response = io.Source.fromURL(s"""http://smsgw0.novatel.si/sms.php?username=${username}&password=${password}&to=${number}&msg_enc_type=base64&msg=$msg64""").mkString.split(" ")
       
@@ -22,6 +29,9 @@ class NovatelSMS(username: String, password: String) {
         case _ => NovatelSMS.responseMap("error")
       }
     } catch {
+      case e: AssertionError=>
+        println(e)
+        NovatelSMS.responseMap("assert")
       case e: Exception =>
         println(e)
         NovatelSMS.responseMap("error")
